1b802adaa4d3374ddbadf9ca556fa979c1178777
Tom J Nowell <contact@tomjn.com>
2018-01-20 23:14:36 +0000

    removed the no-js and no-svg html classes

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
+<html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
