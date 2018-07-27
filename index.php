b16687869f6621019c4de9380b66db69b25a5757
Tom J Nowell <contact@tomjn.com>
2018-07-27 01:14:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
 
 get_header();
 
@@ -55,4 +40,6 @@ $gutenberg_version = $data['Version'];
 		</div>
 	</div>
 </div>
-<?php get_footer();
+<?php
+
+wp_footer();
