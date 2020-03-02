eb78df57d208d4791e5fae3e159f730a73456865
Tom J Nowell <contact@tomjn.com>
2020-03-02 17:46:08 +0000

    Some updates to the main markup

--- index.php
+++ index.php
@@ -34,20 +34,27 @@ get_header();
 	</div>
 	<div id="wpcontent">
 		<div id="wpbody" role="main">
-			<div id="wpbody-content" aria-label="Main content" tabindex="0">
-				<div class="nvda-temp-fix screen-reader-text">&nbsp;</div>
-				<div class="block-editor gutenberg">
-					<div id="editor" class="block-editor__container gutenberg__editor"></div>
+			<div id="wpbody-content">
+				<div class="block-editor">
+					<h1 class="screen-reader-text hide-if-no-js">Edit Post</h1>
+					<div id="editor" class="block-editor__container hide-if-no-js"></div>
 					<div id="metaboxes" class="hidden">
 						<?php /*
 						this is commented out because it causes a fatal with do_metaboxes not being found
 						the_block_editor_meta_boxes(); */ ?>
 					</div>
+					<div class="wrap hide-if-js block-editor-no-js">
+						<h1 class="wp-heading-inline">Edit Post</h1>
+						<div class="notice notice-error notice-alt">
+							<p>The block editor requires JavaScript. Please enable JavaScript in your browser settings</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-	</div>
-</div>
+			<div class="clear"></div></div><!-- wpbody-content -->
+		<div class="clear"></div></div><!-- wpbody -->
+	<div class="clear"></div></div><!-- wpcontent -->
+<div class="clear"></div></div><!-- wpwrap -->
+
 <?php
 
 get_footer();
