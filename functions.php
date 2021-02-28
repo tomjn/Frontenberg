0320219bea78b0649ad9d019f96bcfd0baa181ec
Tom J Nowell <contact@tomjn.com>
2021-02-28 10:32:52 -0500

    fix a remove action

--- functions.php
+++ functions.php
@@ -665,10 +665,10 @@ add_filter( 'jetpack_gutenberg', '__return_false' );
 
 if ( class_exists( 'Jetpack_WPCOM_Block_Editor' ) ) {
 	$jpwpcombe = Jetpack_WPCOM_Block_Editor::init();
-	remove_action( 'enqueue_block_editor_assets', [ $jpwpcombe, 'enqueue_block_editor_assets' ] );
+	remove_action( 'enqueue_block_editor_assets', [ $jpwpcombe, 'enqueue_block_editor_assets' ], 9 );
 	remove_action( 'enqueue_block_assets', [ $jpwpcombe, 'enqueue_block_assets' ] );
 	remove_action( 'mce_external_plugins', [ $jpwpcombe, 'add_tinymce_plugins' ] );
 	remove_action( 'block_editor_settings', 'Jetpack\EditorType\remember_block_editor' );
 	remove_action( 'login_init', [ $jpwpcombe, 'allow_block_editor_login' ] );
-	remove_action( 'enqueue_block_editor_assets', [ $jpwpcombe, '' ] );
 }
+
