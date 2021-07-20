ed2f5db7affd62125e077c5c2d178159943c66a3
Tom J Nowell <contact@tomjn.com>
2021-07-20 15:06:37 -0400

    WP 5.8 fix

--- includes/shims.php
+++ includes/shims.php
@@ -37,50 +37,55 @@ function get_block_editor_server_block_settings() {
 
 	return $blocks;
 }
-function get_block_categories( $post ) {
-	$default_categories = array(
-		array(
-			'slug'  => 'text',
-			'title' => _x( 'Text', 'block category' ),
-			'icon'  => null,
-		),
-		array(
-			'slug'  => 'media',
-			'title' => _x( 'Media', 'block category' ),
-			'icon'  => null,
-		),
-		array(
-			'slug'  => 'design',
-			'title' => _x( 'Design', 'block category' ),
-			'icon'  => null,
-		),
-		array(
-			'slug'  => 'widgets',
-			'title' => _x( 'Widgets', 'block category' ),
-			'icon'  => null,
-		),
-		array(
-			'slug'  => 'embed',
-			'title' => _x( 'Embeds', 'block category' ),
-			'icon'  => null,
-		),
-		array(
-			'slug'  => 'reusable',
-			'title' => _x( 'Reusable Blocks', 'block category' ),
-			'icon'  => null,
-		),
-	);
 
-	/**
-	 * Filters the default array of block categories.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param array[] $default_categories Array of block categories.
-	 * @param WP_Post $post               Post being loaded.
-	 */
-	return apply_filters( 'block_categories', $default_categories, $post );
+
+if ( ! function_exists( 'get_block_categories' ) ) {
+	function get_block_categories( $post ) {
+		$default_categories = array(
+			array(
+				'slug'  => 'text',
+				'title' => _x( 'Text', 'block category' ),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'media',
+				'title' => _x( 'Media', 'block category' ),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'design',
+				'title' => _x( 'Design', 'block category' ),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'widgets',
+				'title' => _x( 'Widgets', 'block category' ),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'embed',
+				'title' => _x( 'Embeds', 'block category' ),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'reusable',
+				'title' => _x( 'Reusable Blocks', 'block category' ),
+				'icon'  => null,
+			),
+		);
+
+		/**
+		 * Filters the default array of block categories.
+		 *
+		 * @since 5.0.0
+		 *
+		 * @param array[] $default_categories Array of block categories.
+		 * @param WP_Post $post               Post being loaded.
+		 */
+		return apply_filters( 'block_categories', $default_categories, $post );
+	}
 }
+
 function wp_check_post_lock( $post_id ) { return false; }
 function wp_set_post_lock() {}
 function add_meta_box() {}
